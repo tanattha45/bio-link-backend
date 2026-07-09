43bce632924cd6426ccc3e8335de82f5e98b60d3
Naphasorn4751 <loveopo16343@gmail.com>
2026-07-09 16:37:00 +0700

    AddStorage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create-symlink', function () {
+    Artisan::call('storage:link');
+    return 'Storage Link Created Successfully!';
 });
